841d6faa71acd6e752e328ef9d3d3e26e370826a
Emmanuel euchin <euchin@i-next.fr>
2024-04-03 14:56:46 +0200

    first commit

--- migrations/Version20240403124506.php
+++ migrations/Version20240403124506.php
@@ -20,8 +20,7 @@ final class Version20240403124506 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE breeder_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE strain_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+
         $this->addSql('CREATE TABLE breeder (id INT NOT NULL, name VARCHAR(255) NOT NULL, url_photo VARCHAR(255) NOT NULL, name_id VARCHAR(255) NOT NULL, img VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE strain (id INT NOT NULL, breeder_id INT NOT NULL, name VARCHAR(255) NOT NULL, name_id VARCHAR(255) NOT NULL, auto BOOLEAN NOT NULL, duration INT DEFAULT NULL, type VARCHAR(255) DEFAULT NULL, description TEXT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_A630CD7233C95BB1 ON strain (breeder_id)');
@@ -32,8 +31,7 @@ final class Version20240403124506 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE breeder_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE strain_id_seq CASCADE');
+
         $this->addSql('ALTER TABLE strain DROP CONSTRAINT FK_A630CD7233C95BB1');
         $this->addSql('DROP TABLE breeder');
         $this->addSql('DROP TABLE strain');
